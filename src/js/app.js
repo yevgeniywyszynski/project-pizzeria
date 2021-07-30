e7d03e586b3298d78a449c76af3b3d614e205782
YevgeniyWyszynski <ywyszynski@gmail.com>
2021-07-30 22:15:46 +0200

    Add sub-page

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,48 @@
-import { settings, select } from './settings.js';
+import { settings, select, classNames } from './settings.js';
 import Product from './componets/Product.js';
 import Cart from './componets/Cart.js';
 
 const app = {
+  initPages: function(){
+    const thisApp = this;
+
+    thisApp.pages = document.querySelector(select.containerOf.pages).children;
+    thisApp.navLinks = document.querySelectorAll(select.nav.links);
+    thisApp.activatePage(thisApp.pages[0].id);
+
+    for(let link of thisApp.navLinks){
+      link.addEventListener('click', function(event){
+        const clickedElement = this;
+        event.preventDefault();
+
+        /* get page id from href atribute */
+        const id = clickedElement.getAttribute('href').replace('#', '');
+        
+        /* run thisApp.activePage with that id */
+
+        thisApp.activatePage(id);
+      });
+    }
+  },
+
+  activatePage: function(pageId){
+    const thisApp = this;
+
+    //add class active to matching pages, remove from non-matching
+    for(let page of thisApp.pages){
+      page.classList.toggle(classNames.pages.active, page.id == pageId);
+    }
+    //console.log(thisApp.navLinks);
+    //add class active to matching links, remove from non-matching
+    for(let link of thisApp.navLinks){
+      link.classList.toggle(
+        classNames.nav.active,
+        link.getAttribute('href') == '#' + pageId
+      );
+    }
+
+  },
+
   initCart: function(){
     const thisApp = this;
 
@@ -52,6 +92,7 @@ const app = {
     // console.log('classNames:', classNames);
     // console.log('settings:', settings);
     // console.log('templates:', templates);
+    thisApp.initPages();
     thisApp.initData();
     thisApp.initCart();
   },
